64a55d9e900345f9a3337b3abd457d2c7f497a5a
Alexander <108339772+Mollivex@users.noreply.github.com>
2024-05-12 22:47:43 +0300

    #Project 01 (Start project)

---
+++ MyProject_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+/*
+ * First console program "Hello, world!"
+ */
+
+namespace MyProject_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
